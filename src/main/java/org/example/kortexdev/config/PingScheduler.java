8dddb59fc6a3851f00b7e24f9c5c1d9ded083bf4
SVEND-1 <s5090@inbox.ru>
2026-06-24 22:55:52 +0300

    Ping Dockerfile

---
+++ src/main/java/org/example/kortexdev/config/PingScheduler.java
@@ -0,0 +1,25 @@
+package org.example.kortexdev.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+public class PingScheduler {
+
+    @Value("${server.url}")
+    private int url;
+
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    @Scheduled(fixedRate = 600000)
+    public void pingSelf() {
+        try {
+            String url = this.url + "/ping";
+            restTemplate.getForObject(url, String.class);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
